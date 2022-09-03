0d3c0bf3d0941705d0e35092b639a9ea6af0dc5d
Evfidiw <evfxie@126.com>
2022-09-03 23:04:52 +0800

    learn

--- code/4_graph/notes/connectivity.md
+++ code/4_graph/notes/connectivity.md
@@ -7,8 +7,80 @@
 ## 强连通分量
 
 - 强连通图：若一张有向图的节点两两互相可达，则称这张图是强连通的
+
+    强连通分量：通俗点说，如果一个图的子图中，任意两点可以相互到达，那么这就组成了一个强联通分量。
+
 - dfs生成树：如果结点u是某个强连通分量在搜索树中遇到的第一个结点，那么这个强连通分量的其余结点肯定是在搜索树中以u为根的子树中。结点u被称为这个强连通分量的根。(可由反证法证明)
 
 - Tarjan算法求强连通分量：
-    - 利用dfs生成树的原理，搜索时把没处理的结点加入一个栈，回溯时判断当前结点能否与栈中结点构成一个强连通分量。分别找到一条条回路，使得初始点又回到初始点。
-    - 
+
+    - 思路：利用dfs生成树的原理，搜索时把没处理的结点加入一个栈，回溯时判断当前结点能否与栈中结点构成一个强连通分量。分别找到一条条回路，使得初始点又回到初始点。
+    - 步骤：
+        1. 记录结点u的搜索次序编号（时间戳），以及结点u或者u的子树最多能够回溯到的结点x 且使得时间戳最小。
+        2. 将u入栈。
+        3. 同时在图中更新枚举的边。
+        4. 如果形成了一条回路（即弹出u） ，此时被弹出的所有节点形成了一个强连通分量。
+
+- 模板参考：
+
+    ```c++
+    // dfn:时间戳
+    // low:记录u能回溯到哪个点 
+    
+    int idx=0, num=0, top=0, col;
+    int head[N], to[N*5], nxt[N*5]; 
+    int dfn[N], low[N], st[N], co[N];
+    
+    void init() 
+    {
+    	idx = num = top = 0;
+    	memset(head, -1, sizeof head);
+    }
+    
+    void add(int u,int v)
+    {
+    	to[++idx]=v, nxt[idx]=head[u], head[u]=idx;
+    }
+    
+    void Tar(int u)
+    {
+    	dfn[u] = low[u] = ++num;
+    	st[++top] = u;
+    	for(int i=head[u]; i!=-1; i=nxt[i]){
+    		int v = to[i];
+    		if(!dfn[v]){
+    			Tar(v);
+    			low[u] = min(low[u],low[v]);
+    		} 
+    		else if(!co[v]) low[u] = min(low[u],dfn[v]);
+    	}
+    	if(low[u] == dfn[u]){
+    		co[u] = ++col;
+    		while(st[top]!=u){
+    			co[st[top]] = col;
+    			top--;
+    		}
+    		top--;
+    	}
+    }
+    
+    for (int i = 1; i <= n; i++){
+    	if (!dfn[i]) Tar(i);
+    }
+    printf ("%lld\n", col);	//有col个强连通分量
+    for (int i = 1; i <= n; i++){
+    	if (vis[i]) continue;
+    	printf ("%lld ", i);
+    	vis[i] = 1;
+    	for (int j=i+1; j<=n; j++){
+    		if (co[j] == co[i]){
+    			printf ("%lld ", j);
+    			vis[j] = 1;
+    		}
+    	}
+    }
+    ```
+
+- eg：
+
+    [eg1](https://github.com/Evfidiw/acm-blog/blob/main/code/4_graph/topics/luoguP2341.cpp)	
--- code/4_graph/topics/luoguP2341.cpp
+++ code/4_graph/topics/luoguP2341.cpp
@@ -6,58 +6,63 @@
 	若出现两个以上出度为0的强连通分量则不存在明星奶牛，因为没有满足所有人都喜欢。 
 */ 
 
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
 using namespace std;
-#define ll long long
-#define inf 0x3f3f3f3f
-char nc(){
+char getc(){
     static char buf[1<<14],*p1=buf,*p2=buf;
     return (p1==p2)&&(p2=(p1=buf)+fread(buf,1,1<<14,stdin),p1==p2)?EOF:*p1++;
 }
 int read(){
-    int s=0,w=1;char ch=nc();
-    while(ch<'0'||ch>'9'){if(ch=='-')w=-1;ch=nc();}
-    while(ch>='0'&&ch<='9')s=s*10+ch-'0',ch=nc();
-    return s*w;
+   int s=0,w=1;  
+   char ch=getchar();  
+   while(ch<'0'||ch>'9'){if(ch=='-')w=-1;ch=getchar();}  
+   while(ch>='0'&&ch<='9') s=s*10+ch-'0',ch=getchar();  
+   return s*w;  
+}
+#define ll long long
+#define inf 0x3f3f3f3f
+const int N=10005;
+
+int n,m;
+ll ans=0;
+int idx=0, num=0, top=0, col;
+int head[N], to[N*5], nxt[N*5]; 
+int dfn[N], low[N], st[N], co[N];
+int si[N], de[N];
+
+void init() 
+{
+	idx = num = top = 0;
+	memset(head, -1, sizeof head);
 }
 
-const int maxn=10005;
-struct Node{
-	int v,from;
-};
-
-int n,m,cnt,num,top,col;
-ll ans;
-int fir[maxn],dfn[maxn],low[maxn],st[maxn],co[maxn],si[maxn],de[maxn];
-Node e[maxn*5];
-	
 void add(int u,int v)
 {
-	e[++cnt].v=v,e[cnt].from=fir[u],fir[u]=cnt;
+	to[++idx]=v, nxt[idx]=head[u], head[u]=idx;
 }
 
 void Tar(int u)
 {
-	dfn[u]=low[u]=++num;
-	st[++top]=u;
-	for(int i=fir[u];i;i=e[i].from){
-		int v=e[i].v;
+	dfn[u] = low[u] = ++num;
+	st[++top] = u;
+	for(int i=head[u]; i!=-1; i=nxt[i]){
+		int v = to[i];
 		if(!dfn[v]){
 			Tar(v);
-			low[u]=min(low[u],low[v]);
-		} else 
-		if(!co[v]) low[u]=min(low[u],dfn[v]);
+			low[u] = min(low[u],low[v]);
+		} 
+		else if(!co[v]) low[u] = min(low[u],dfn[v]);
 	}
-	if(low[u]==dfn[u]){
-		co[u]=++col;
+	if(low[u] == dfn[u]){
+		co[u] = ++col;
 		si[col]++;
 		while(st[top]!=u){
 			si[col]++;
-			co[st[top]]=col;
+			co[st[top]] = col;
 			top--;
 		}
 		top--;
@@ -69,21 +74,22 @@ int main(){
 //    freopen(".out","w",stdout);
 //	printf("%.3lf M\n",(double)sizeof(/*szm*/)/(1<<20));
 //	clock_t start_time=clock();
-    n=read(),m=read();
+	init();
+    scanf("%d%d",&n,&m);
     for(int i=1; i<=m; i++){
-    	int x=read(),y=read();
+    	int x=read(), y=read();
     	add(y,x);
     }
     for(int i=1; i<=n; i++){
     	if(!dfn[i]) Tar(i);
     }
     for(int i=1; i<=n; i++){
-    	for(int j=fir[i];j;j=e[j].from)
-    		if(co[i]!=co[e[j].v]) de[co[e[j].v]]++;
+    	for(int j=head[i]; j!=-1; j=nxt[j])
+    		if(co[i] != co[to[j]]) de[co[to[j]]]++;
     }
 	int tot=0;
     for(int i=1; i<=col; i++){
-    	if(!de[i]) ans=si[i],tot++;
+    	if(!de[i]) ans=si[i], tot++;
     }
     if(tot==1) printf("%lld\n",ans);
     else puts("0");
